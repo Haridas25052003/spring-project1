0163b384c69ca3d482a05733568c0ac4b4e3cd22
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 21:41:33 +0530

    added the rowmapper class

--- src/main/java/com/util/StudentRowMapper.java
+++ src/main/java/com/util/StudentRowMapper.java
@@ -1,11 +1,21 @@
 package com.util;
 
-//import javax.swing.tree.RowMapper;
-//
-//public class StudentRowMapper implements RowMapper<Student>{
-//
-//	@override
-//	public Student(ResultSet rs,int rowNum) throws SQLException{
-//		
-//	}
-//}
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.model.Student;
+
+public class StudentRowMapper implements RowMapper<Student>{
+	
+	public Student mapRow(ResultSet rs,int rowNum) throws SQLException{
+		Student s=new Student();
+		s.setId(rs.getInt("id"));
+		s.setName(rs.getString("name"));
+		s.setCity(rs.getString("city"));
+		s.setPercentage(rs.getDouble("percentage"));
+		
+		return s;
+	}
+}
