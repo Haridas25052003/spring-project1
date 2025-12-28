ac29974e8d105b9b56a0d0a5f61bb07fd0485cb3
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 15:13:37 +0530

    added the add.main file

--- src/main/java/spring_project1/spring_project1/App.java
+++ src/main/java/spring_project1/spring_project1/App.java
@@ -1,5 +1,11 @@
 package spring_project1.spring_project1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.Student;
+import com.utils.StudentDao;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+        
+        StudentDao sd=context.getBean(StudentDao.class);
+        
+        Student s=new Student();
+        
+        s.setName("alex jordan");
+    	s.setCity("basamth");
+    	s.setPercentage(56.89);
+    	
+    	System.out.println(sd.insertStudent(s));
     }
 }
