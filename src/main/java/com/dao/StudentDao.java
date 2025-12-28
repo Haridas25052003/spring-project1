6c70c968e91428240b460f8c067938186e33c289
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 22:00:56 +0530

    add the changes

--- src/main/java/com/dao/StudentDao.java
+++ src/main/java/com/dao/StudentDao.java
@@ -1,8 +1,13 @@
 package com.dao;
 
+
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.model.Student;
+import com.util.StudentRowMapper;
+
 
 public class StudentDao {
 	
@@ -12,6 +17,7 @@ public class StudentDao {
 		this.j = j;
 	}
 
+	//Student s=new Student();
 	public int insertStudent(Student s) {
 		String s1="insert into student (name, city,percentage) values (?,?,?)";
 		
@@ -23,4 +29,21 @@ public class StudentDao {
 		return j.update(sql,id);
 	}
 
+	public Student findStudentById(int id) {
+		String sql="select id,name,city,percentage from student where id=?";
+		
+		List<Student> list=j.query(sql,new StudentRowMapper(),id);
+		return (list.isEmpty())? null:list.get(0);
+	}
+	
+	public List<Student> findAllStudent(){
+		String sql="delete id,name,city,percentage from student";
+		
+		return j.query(sql,new StudentRowMapper());
+	}
+	
+	public int updateStudent(Student s) {
+		String sql="update Student set name=? , city=? , percentage=? where id=?";
+		return j.update(sql,new Object[] {s.getName(),s.getCity(),s.getPercentage(),s.getId() });
+	}
 }
