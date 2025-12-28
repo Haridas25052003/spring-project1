448c58550b0da7a66f1d14c7b6828f1ac652d120
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 16:09:37 +0530

    add files

---
+++ src/main/java/com/util/StudentRowMapper.java
@@ -0,0 +1,11 @@
+package com.util;
+
+import javax.swing.tree.RowMapper;
+
+public class StudentRowMapper implements RowMapper<Student>{
+
+	@override
+	public Student(ResultSet rs,int rowNum) throws SQLException{
+		
+	}
+}
